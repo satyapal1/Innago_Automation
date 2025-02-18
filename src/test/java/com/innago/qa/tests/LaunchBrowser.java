a47fbd0b5c50d4c1d790f7bfdf578ad07aae938c
satyapal.kumar <satyapal.kumar@taazaa.com>
2025-02-18 12:11:22 +0530

    adding code for launch browser

--- src/test/java/com/innago/qa/tests/LaunchBrowser.java
+++ src/test/java/com/innago/qa/tests/LaunchBrowser.java
@@ -1,9 +1,14 @@
 package com.innago.qa.tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LaunchBrowser {
 
 	public static void main(String[] args) {
-		  
+     WebDriver driver;
+		
+		driver = new ChromeDriver();
 
 	}
 
